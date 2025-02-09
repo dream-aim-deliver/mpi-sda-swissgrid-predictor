ccb7961f06dca46f86dd1b828d5ccc3b0c92b743
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-02-09 19:00:20 +0100

    feat(API): pass model name as the /predict request body, instead of an env variable; improve response JSON

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 from flask import Flask, request, jsonify
 import tensorflow as tf
 from tensorflow.keras.models import load_model
@@ -6,21 +7,30 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 
-MODEL_BASEPATH = os.getenv("MODEL_BASEPATH")
-MODEL_FILENAME = os.getenv("MODEL_FILENAME")
+MODEL_BASEPATH = "/model_files"
+SUPPORTED_MODELS = ["unified", "beznau"]
 KP_HOST = os.getenv("KP_HOST")
 KP_PORT = os.getenv("KP_PORT")
 KP_SCHEME = os.getenv("KP_SCHEME")
 KP_AUTH_TOKEN = os.getenv("KP_AUTH_TOKEN")
 
 
-if any(v is None for v in [MODEL_BASEPATH, MODEL_FILENAME, KP_HOST, KP_PORT, KP_SCHEME, KP_AUTH_TOKEN]):
-    raise ValueError("Please set all environment variables: MODEL_BASEPATH, MODEL_FILENAME, KP_HOST, KP_PORT, KP_SCHEME, KP_AUTH_TOKEN")
+if any(v is None for v in [KP_HOST, KP_PORT, KP_SCHEME, KP_AUTH_TOKEN]):
+    raise ValueError("Please set all environment variables: KP_HOST, KP_PORT, KP_SCHEME, KP_AUTH_TOKEN")
+
+
+def probability_to_prediction(probability: int) -> Literal["ON", "OFF"]:
+    return "ON" if probability > 0.5 else "OFF"
+
+def probability_to_confidence(probability: int) -> float:
+    return probability if probability > 0.5 else 1 - probability
+
 
 
 app = Flask(__name__)
 with tf.device("/CPU:0"):
-    model = load_model(os.path.join(MODEL_BASEPATH, MODEL_FILENAME))
+    unified_model = load_model(os.path.join(MODEL_BASEPATH, "Unified_model.keras"))
+    beznau_model = load_model(os.path.join(MODEL_BASEPATH, "Unified_Beznau_model.keras"))
 
 
 @app.route('/')
@@ -43,6 +53,11 @@ def predict():
         if len(images) != 5:
             return jsonify({"error": f"Exactly 5 images required, Received {len(images)}."}),400
 
+        original_model_name = data['model_name']
+        model_name = original_model_name.strip().lower()
+        if model_name not in SUPPORTED_MODELS:
+            return jsonify({"error": f"Invalid model name '{original_model_name}'. Please choose from {SUPPORTED_MODELS}"}), 400
+
         preprocessed_images = []
         target_size = (256, 256)
        
@@ -53,17 +68,57 @@ def predict():
         combined_images = np.concatenate(preprocessed_images, axis=-1)
 
         # Make predictions
-        predictions = model.predict(np.expand_dims(combined_images, axis=0))  # Add batch dimension
+        if model_name == 'unified':
+            predictions = unified_model.predict(np.expand_dims(combined_images, axis=0))  # Add batch dimension
 
-        if MODEL_FILENAME == 'Unified_Beznau_model.keras':
-            predictions = [pred.tolist() for pred in predictions]
-            return jsonify({'predictions': predictions})
+            probability = predictions.tolist()[0][0]
 
-        elif MODEL_FILENAME == 'Unified_model.keras':
-            return jsonify({'predictions': predictions.tolist()})
+            prediction = probability_to_prediction(probability) 
+            confidence = probability_to_confidence(probability)
+
+            return jsonify({
+                'data': [
+                    {
+                        'label': model_name,
+                        'prediction': prediction,
+                        'confidence': confidence
+                    }
+                ]
+            })
+                
+
+        elif model_name == 'beznau':
+            predictions = beznau_model.predict(np.expand_dims(combined_images, axis=0)) # Add batch dimension
+            nested_prob_1, nested_prob_2 = (pred.tolist() for pred in predictions)
+
+            probability_1 = nested_prob_1[0][0]
+            prediction_1 = probability_to_prediction(probability_1) 
+            confidence_1 = probability_to_confidence(probability_1) 
+
+            probability_2 = nested_prob_2[0][0]
+            prediction_2 = probability_to_prediction(probability_2)
+            confidence_2 = probability_to_confidence(probability_2) 
+
+            return jsonify({
+                'data': [
+                    {
+                        'label': f"{model_name}_tower_1",
+                        'prediction': prediction_1,
+                        'confidence': confidence_1
+                    },
+                    {
+                        'label': f"{model_name}_tower_2",
+                        'prediction': prediction_2,
+                        'confidence': confidence_2
+                    }
+                ]
+            })
+
+        else:
+            return jsonify({"error": f"Invalid model name. Please choose from {SUPPORTED_MODELS}"}), 400
 
     except Exception as e:
-        print("Error during prediction:", str(e))
+        print("Error during prediction: ", str(e))
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # Load the environment variables from .env
 echo "=> Loading environment variables from .env file..."
 set -a
@@ -8,23 +7,8 @@ source .env
 set +a
 
 # Check that all environment variables are set
-if [ -z "$MODEL_BASEPATH" ] || [ -z "$MODEL_FILENAME" ] || [ -z "$KP_HOST" ] || [ -z "$KP_PORT" ] || [ -z "$KP_SCHEME" ] || [ -z "$KP_AUTH_TOKEN" ]; then
+if [ -z "$KP_HOST" ] || [ -z "$KP_PORT" ] || [ -z "$KP_SCHEME" ] || [ -z "$KP_AUTH_TOKEN" ]; then
     echo "MODEL_BASEPATH, MODEL_FILENAME, KP_HOST, KP_PORT, KP_SCHEME and KP_AUTH_TOKEN must be set in the .env file. Aborting."
-    exit 1
-fi
-
-if [ "$MODEL_FILENAME" == "Unified_Beznau_model.keras" ] || [ "$MODEL_FILENAME" == "Unified_model.keras" ]; then
-
-    if [ "$MODEL_FILENAME" == "Unified_Beznau_model.keras" ]; then
-        MODEL="Beznau"
-
-    else
-        MODEL="Unified"
-
-    fi
-
-else
-    echo "The MODEL_FILENAME must be either 'Unified_Beznau_model.keras' or 'Unified_model.keras'. Aborting."
     exit 1
 fi
 
@@ -33,14 +17,12 @@ echo "=> Successfully loaded environment variables from .env file."
 
 # Run the docker container
 echo "=> Running the docker container..."
-CONTAINER_NAME="mpi-sda-swissgrid-predictor-${MODEL}"
-printf "=> Container name: ${CONTAINER_NAME}\n=> Model: ${MODEL}\n"
+CONTAINER_NAME="mpi-sda-swissgrid-predictor"
+printf "=> Container name: ${CONTAINER_NAME}\n"
 
 docker run --rm \
     --name "${CONTAINER_NAME}" \
     -p 5000:5000 \
-    -e MODEL_BASEPATH="${MODEL_BASEPATH}" \
-    -e MODEL_FILENAME="${MODEL_FILENAME}" \
     -e KP_HOST="${KP_HOST}" \
     -e KP_PORT="${KP_PORT}" \
     -e KP_SCHEME="${KP_SCHEME}" \
